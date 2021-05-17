c0037a6a261f3ce42c3f85b6130a94b1e3939443
Harsh542 <65009685+Harsh542@users.noreply.github.com>
2021-05-17 19:19:49 +0530

    Update utils.py

--- ChatApp/utils.py
+++ ChatApp/utils.py
@@ -3,14 +3,14 @@ from twilio.rest import Client
 # Create your models here.
 
 
-account_sid = 'AC6697aa51bc8b11f12c6e4e306877a206'
-auth_token = '8e6ce924e078b86af01c70ec78c68718'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 
 def send_otp(number, otp):
     message = client.messages.create(
         body=f'Hi there! your otp is - {otp}',
-        from_='+17372154524',
+        from_='',
         to=f'+91{number}'
     )
